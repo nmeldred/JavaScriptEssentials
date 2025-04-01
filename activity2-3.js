3e7740a04c057345a11726f2e1bf04a49a6e9a6e
nmeldred <nmeldred1@gmail.com>
2025-04-02 00:11:10 +0100

    Add files via upload
    Activity 2.3: Log some information to the console of the web browser

---
+++ activity2-3.js
@@ -0,0 +1,9 @@
+var temperature = 25;
+
+if (temperature >= 30) {
+    console.log("It's a boiling day.");
+} else if (temperature >= 20 && temperature < 30) {
+    console.log("It's a hot day.");
+} else {
+    console.log("It's a mild day.");
+}
